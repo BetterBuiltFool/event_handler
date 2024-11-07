1062b80f58bb675ed0b884126974bc77b2af6f50
BetterBuiltFool <betterbuiltfool@gmail.com>
2024-11-07 12:46:16 -0500

    Attempts fix for import issues

--- src/event_handler/__init__.py
+++ src/event_handler/__init__.py
@@ -1,2 +1,2 @@
-from src.event_handler.event_manager import getEventManager, notifyEventManagers  # noqa: F401, E501
-from src.event_handler.key_manager import getKeyListener, notifyKeyListeners  # noqa: F401, E501
+from .event_manager import getEventManager, notifyEventManagers  # noqa: F401, E501
+from .key_manager import getKeyListener, notifyKeyListeners  # noqa: F401, E501
