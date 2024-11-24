712b09f411411406adc244f1946690cc15ca0faf
BetterBuiltFool <betterbuiltfool@gmail.com>
2024-11-24 17:38:56 -0500

    Adds basicConfig which passes on to other configs

--- src/simple_events/__init__.py
+++ src/simple_events/__init__.py
@@ -1,8 +1,13 @@
 import sys
-from .base_manager import basicConfig  # noqa: F401
+from .base_manager import managerBasicConfig  # noqa: F401
 from .event_manager import getEventManager, notifyEventManagers  # noqa: F401, E501
 from .key_manager import getKeyListener, notifyKeyListeners  # noqa: F401, E501
 from .file_parser import JSONParser  # noqa: F401, E501
+
+
+def basicConfig(*args, **kwds):
+    managerBasicConfig(*args, **kwds)
+
 
 if sys.platform == "emscripten":
     # We always want to run in async mode if we're online
