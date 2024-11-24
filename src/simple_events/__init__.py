ebac5ac2a293908b294f1f7827c43cbccf2a1a93
BetterBuiltFool <betterbuiltfool@gmail.com>
2024-11-24 13:38:09 -0500

    Adds online awareness and import basicConfig

--- src/simple_events/__init__.py
+++ src/simple_events/__init__.py
@@ -1,3 +1,9 @@
+import sys
+from .base_manager import basicConfig  # noqa: F401
 from .event_manager import getEventManager, notifyEventManagers  # noqa: F401, E501
 from .key_manager import getKeyListener, notifyKeyListeners  # noqa: F401, E501
 from .file_parser import JSONParser  # noqa: F401, E501
+
+if sys.platform == "emscripten":
+    # We always want to run in async mode if we're online
+    basicConfig(is_async=True)
