384e8f85a82e9de01130f258e0da86230d5fecc6
BetterBuiltFool <betterbuiltfool@gmail.com>
2024-11-25 13:21:06 -0500

    Removes inappropriate auto config Should not autoconfig to run async if it detects WASM runtime. The client should be responsible for this. Auto config means the library will behave differently in an uncontrolled way, making it harder to debug.

--- src/simple_events/__init__.py
+++ src/simple_events/__init__.py
@@ -1,4 +1,3 @@
-import sys
 from .base_manager import managerBasicConfig  # noqa: F401
 from .event_manager import getEventManager, notifyEventManagers  # noqa: F401, E501
 from .key_manager import getKeyListener, notifyKeyListeners  # noqa: F401, E501
@@ -7,8 +6,3 @@ from .file_parser import JSONParser  # noqa: F401, E501
 
 def basicConfig(*args, **kwds):
     managerBasicConfig(*args, **kwds)
-
-
-if sys.platform == "emscripten":
-    # We always want to run in async mode if we're online
-    basicConfig(is_async=True)
